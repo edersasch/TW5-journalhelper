1a3155a01ce42569f758c309c07faf10a2a99462
Alexander Eder <edersasch@gmx.de>
2024-03-23 22:50:07 +0100

    javascript macros: filter for short dayname, document

--- plugins/journalhelper/journalfilter.js
+++ plugins/journalhelper/journalfilter.js
@@ -3,7 +3,19 @@ title: $:/plugins/edersasch/journalhelper/journalfilter.js
 type: application/javascript
 module-type: macro
 
-special tiddler filtering
+Show a list of journal tiddlers that match a set of filter fields
+
+* parameter tmp: temporary tiddler to use for field lookup, default is `$:/temp/journalfilter`
+* parameter filter_year_field: name of the field to look for year filter number, default is `Filter Year`
+* parameter filter_month_field: name of the field to look for month filter number, default is `Filter Month`
+* parameter filter_day_field: name of the field to look for day filter number, default is `Filter Day`
+* parameter filter_dayname_field: name of the field to look for short name of weekday filter string, default is `Filter Dayname`
+* parameter filter_title_content_field: unused, default is `Filter Title Content`
+
+Every set filter field must match the corresponding part of the field `created`
+of every non draft tiddler tagged with `Journal`.
+
+A `Journal` tiddler not having a `created` field will be shown.
 
 \*/
 (function(){
@@ -23,21 +35,24 @@ exports.params = [
     {name: "filter_year_field",             default: "Filter Year"},
     {name: "filter_month_field",            default: "Filter Month"},
     {name: "filter_day_field",              default: "Filter Day"},
+    {name: "filter_dayname_field",          default: "Filter Dayname"},
     {name: "filter_title_content_field",    default: "Filter Title Content"}
 ];
 
 /*
 Run the macro
 */
-exports.run = function(tmp, filter_year_field, filter_month_field, filter_day_field, filter_title_content_field) {
+exports.run = function(tmp, filter_year_field, filter_month_field, filter_day_field, filter_dayname_field, filter_title_content_field) {
     let filter_year = "";
     let filter_month = "";
     let filter_day = "";
+    let filter_dayname = "";
     const tmptid = this.wiki.getTiddler(tmp);
     if (tmptid) {
         filter_year = tmptid.getFieldString(filter_year_field);
         filter_month = tmptid.getFieldString(filter_month_field);
         filter_day = tmptid.getFieldString(filter_day_field);
+        filter_dayname = tmptid.getFieldString(filter_dayname_field);
     }
     const matching_list_filter = "[tag[Journal]!has[draft.of]]";
     const journal_tiddlers = this.wiki.filterTiddlers(matching_list_filter);
@@ -45,12 +60,23 @@ exports.run = function(tmp, filter_year_field, filter_month_field, filter_day_fi
     for (let i in journal_tiddlers) {
         const journalentry = this.wiki.getTiddler(journal_tiddlers[i]);
         if (journalentry) {
-            const journaldate = journalentry.getFieldString("journaldate");
-            let match = journaldate === "";
+            const created = journalentry.getFieldString("created");
+            let match = created === "";
+            /*
+             * Filtering the tiddlers
+             *
+             * Show if the tiddler has no field "created"
+             * else
+             *     hide if a year filter is present and it is not part of the created year
+             *     else
+             *         hide if a month or day filter is present and does not match
+             *         else
+             *             hide if a dayname filter is present and does not match
+             */
             if (match === false) {
-                const jyear = journaldate.substring(0, 4);
-                const jmonth = journaldate.substring(5, 7);
-                const jday = journaldate.substring(8, 10);
+                const jyear = created.substring(0, 4);
+                const jmonth = created.substring(4, 6);
+                const jday = created.substring(6, 8);
                 match = filter_year === "" || jyear.includes(filter_year);
                 function matchByPositiveNumber(dateSubstring, filterString) {
                     if (match && filterString !== "") {
@@ -63,6 +89,13 @@ exports.run = function(tmp, filter_year_field, filter_month_field, filter_day_fi
                 }
                 matchByPositiveNumber(jmonth, filter_month);
                 matchByPositiveNumber(jday, filter_day);
+                if (match && filter_dayname !== "") {
+                    const dayname_filter = "[[" + created + "]format:date[ddd]]";
+                    const dayname = this.wiki.filterTiddlers(dayname_filter);
+                    if (dayname.length === 1) {
+                        match = match && dayname[0].toLowerCase().includes(filter_dayname.toLowerCase());
+                    }
+                }
             }
             if (match) {
                 titles += " [[" + journal_tiddlers[i] + "]]";
@@ -72,16 +105,14 @@ exports.run = function(tmp, filter_year_field, filter_month_field, filter_day_fi
     titles += " +[!sort[created]]";
     let ret = '<$list filter="""' + titles + '""">';
     ret += '<div>';
-    ret += '<$let yearstr={{{ [<currentTiddler>get[journaldate]split[-]nth[1]] }}} monthstr={{{ [<currentTiddler>get[journaldate]split[-]nth[2]] }}} daystr={{{ [<currentTiddler>get[journaldate]split[-]nth[3]] }}}>';
-    ret += '<$edit-text field="doesnotexist" placeholder=<<yearstr>> disabled="yes" class="inputyear" /> / ';
-    ret += '<$edit-text field="doesnotexist" placeholder=<<monthstr>> disabled="yes" class="inputmonthday" /> / ';
-    ret += '<$edit-text field="doesnotexist" placeholder=<<daystr>> disabled="yes" class="inputmonthday" /> ';
-    ret += '<code><$view field="created" format="date" template="ddd" /></code> ';
+    ret += '<$edit-text field="doesnotexist" placeholder={{{ [{!!created}format:date[YYYY]] }}} disabled="yes" class="inputyear" /> / ';
+    ret += '<$edit-text field="doesnotexist" placeholder={{{ [{!!created}format:date[0MM]] }}} disabled="yes" class="inputmonthday" /> / ';
+    ret += '<$edit-text field="doesnotexist" placeholder={{{ [{!!created}format:date[0DD]] }}} disabled="yes" class="inputmonthday" /> ';
+    ret += '<$edit-text field="doesnotexist" placeholder={{{ [{!!created}format:date[ddd]] }}} disabled="yes" class="inputmonthday" /> ';
     ret += '<$link to=<<currentTiddler>>>';
     ret += '<$reveal state="!!caption" type="match" text=""><$view field="title"/></$reveal>';
     ret += '<$reveal state="!!caption" type="nomatch" text=""><$view field="caption"/></$reveal>';
     ret += '</$link>';
-    ret += '</$let>';
     ret += '</div>';
     ret += '</$list>';
     return ret;
--- plugins/journalhelper/syncselectabledate.js
+++ plugins/journalhelper/syncselectabledate.js
@@ -3,8 +3,62 @@ title: $:/plugins/edersasch/journalhelper/syncselectabledate.js
 type: application/javascript
 module-type: macro
 
-synchronize the date content of a field with standard tiddler date content
-and a date for an 'edit-text type="date"' format YYYY-MM-DD
+Synchronizes the date content of a field with standard tiddler date content and
+a date for an `edit-text type="date"` format YYYY-MM-DD
+
+This macro does not return any text, but modifies tiddler fields, which means
+you must
+[[take care where to call it|https://tiddlywiki.com/dev/#JavaScript%20Macros]].
+The examples below work.
+
+* parameter tiddler: use the fields from the tiddler with the given title, default is currentTiddler
+* parameter datefield: a field with a format like a [[tiddlywiki date field|https://tiddlywiki.com/#Date%20Fields]], default is created
+* parameter selectabledatefield: a field with a format for an `edit-text type="date"` format YYYY-MM-DD, default is journaldate
+
+If the tiddler does not exist or both date fields are missing, the macro does
+nothing. On existing datefield but missing selectabledatefield the latter is
+created with the content from the former. If the selectabledatefield is
+present and it differs from the date part of the content of datefield, this
+part is updated.
+
+Example to create a date edit in the edit view for the default field
+`journaldate` in a tiddler tagged with "Journal":
+
+```
+list-after: $:/plugins/edersasch/journalhelper/ui/EditTemplate/journalcaption
+tags: $:/tags/EditTemplate
+title: $:/plugins/edersasch/journalhelper/ui/EditTemplate/journaldate
+
+<$list filter="[<currentTiddler>tag[Journal]]">
+<<syncselectabledate>>
+<div class="tc-edit-tags">
+<$edit-text field="journaldate" type="date" />
+</div>
+</$list>
+```
+
+If `journaldate` has its own editor it does not need a default field editor.
+Note that no end-of-line must be present after `hide`.
+
+```
+title: $:/config/EditTemplateFields/Visibility/journaldate
+
+hide
+```
+
+Example for viewing the selected date in human readable form in tiddlers tagged
+with "Journal":
+
+```
+list-before: $:/core/ui/ViewTemplate/title
+tags: $:/tags/ViewTemplate
+title: $:/plugins/edersasch/journalhelper/ui/ViewTemplate/journalcreated
+
+<$list filter="[<currentTiddler>tag[Journal]]">
+<<syncselectabledate>>
+<$view field="created" format="date" template="DDD, DDth MMM YYYY" />
+</$list>
+```
 
 \*/
 (function(){
